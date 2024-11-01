c882df47759d89c3d5a5c7dad1fbb20467cd5e26
Martin Povišer <povik@cutebit.org>
2024-11-01 15:05:29 +0100

    Add root scope accessor on Compilation

--- source/ast/Compilation.cpp
+++ source/ast/Compilation.cpp
@@ -2708,4 +2708,8 @@ Diagnostic* Compilation::errorMissingDef(std::string_view name, const Scope& sco
     return &diag;
 }
 
+const Scope* Compilation::getRootScope() const {
+    return root.get();
+}
+
 } // namespace slang::ast
